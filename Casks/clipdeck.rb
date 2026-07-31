8d940e3c96b860b71488c5ae2a7d7e5f8c0c5577
ashutoshb206 <ashutoshbansalcs@gmail.com>
2026-07-31 22:55:39 +0530

    clipdeck 1.2.0

--- Casks/clipdeck.rb
+++ Casks/clipdeck.rb
@@ -1,6 +1,6 @@
 cask "clipdeck" do
-  version "1.1.0"
-  sha256 "efa985a34b94bfec023d5b56b6d4be143590ac6517aea7208ca36222739173b3"
+  version "1.2.0"
+  sha256 "f5f8fc258779101e90d20dc11c0efc0f81fade219bd3c10889704d6012109b58"
 
   url "https://clipdeck-mac.vercel.app/downloads/ClipDeck.dmg"
   name "ClipDeck"
